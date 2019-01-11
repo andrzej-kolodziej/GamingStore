8d3e7aaf728792d671fc342a8612a084aac005dd
Andrzej Kolodziej <andrzej.kolodziej@motorolasolutions.com>
2019-01-11 10:37:55 +0100

    add bundle repository test

--- src/test/java/com/app/controllers/AdminControllerTest.java
+++ src/test/java/com/app/controllers/AdminControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -17,14 +18,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-
+@WebMvcTest
 @AutoConfigureMockMvc
 public class AdminControllerTest {
-    private MockMvc mockMvc;
-
     @Autowired
-    private WebApplicationContext webApplicationContext;
-
+    private MockMvc mockMvc;
 
     @Test
     public void should_return_200_status() throws Exception {
--- src/test/java/com/app/repository/BundleRepositoryTest.java
+++ src/test/java/com/app/repository/BundleRepositoryTest.java
@@ -1,8 +1,66 @@
 package com.app.repository;
 
+import com.app.domain.Bundle;
+import com.app.domain.Developer;
+import com.app.domain.Product;
+import com.app.domain.Publisher;
+import com.app.repositories.BundleRepository;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class BundleRepositoryTest {
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private BundleRepository bundleRepository;
+
+    @Test
+    public void whenFindById_thenReturnBundle() {
+        //given
+        Bundle bundle = new Bundle();
+
+        Set<Product> products = new HashSet<>();
+
+        Developer developer = new Developer();
+        developer.setDescription("description");
+        developer.setImageUrl("https://dummyimage.com/300");
+        developer.setName("name");
+
+        Publisher publisher = new Publisher();
+        publisher.setDescription("description");
+        publisher.setImageUrl("https://dummyimage.com/300");
+        publisher.setName("name");
+
+        Product product = new Product();
+        product.setDeveloper(developer);
+        product.setPublisher(publisher);
+        product.setYoutubeUrl("url");
+
+        products.add(product);
+
+        bundle.setProducts(products);
+        testEntityManager.persist(bundle);
+        testEntityManager.flush();
+
+        //when
+        Bundle found = bundleRepository.findById(bundle.getId()).get();
+
+        //then
+        assertThat(found.getId()).isEqualTo(bundle.getId());
+    }
 }
