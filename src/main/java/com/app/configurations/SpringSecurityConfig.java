fdac1dc555d3844c156663346bd6545ade0ce99f
Andrzej Kolodziej <andrzej.kolodziej@motorolasolutions.com>
2019-01-11 11:11:56 +0100

    remove not used dependency; add db to gitignore

--- src/main/java/com/app/configurations/SpringSecurityConfig.java
+++ src/main/java/com/app/configurations/SpringSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.app.configurations;
 
 
-import org.jasypt.util.password.PasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Qualifier;
--- src/main/java/com/app/services/security/EncryptionServiceImpl.java
+++ src/main/java/com/app/services/security/EncryptionServiceImpl.java
@@ -1,6 +1,5 @@
 package com.app.services.security;
 
-import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
