3f44c2d4315c20f53109e303c68de4e1807625c4
Andrzej Kolodziej <andrzej.kolodziej@motorolasolutions.com>
2019-01-15 14:56:06 +0100

    extract customer controller test methods to integration customer controller test

--- src/test/java/com/app/controllers/CustomerControllerTest.java
+++ src/test/java/com/app/controllers/CustomerControllerTest.java
@@ -1,8 +1,10 @@
 package com.app.controllers;
 
+
 import com.app.commands.UserForm;
 import com.app.configuration.SpringSecurityTestConfig;
 import com.app.configurations.SpringSecurityConfig;
+import com.app.domain.Address;
 import com.app.domain.Role;
 import com.app.domain.User;
 import com.app.services.*;
@@ -15,10 +17,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import sun.security.acl.PrincipalImpl;
+
+import java.security.Principal;
+
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -28,11 +35,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        classes = {SpringSecurityTestConfig.class, /*SpringSecurityConfig.class*/})
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+        /*classes = {SpringSecurityTestConfig.class, SpringSecurityConfig.class}*/)
 @TestPropertySource(
         locations = "classpath:application.properties")
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(secure = true)
 public class CustomerControllerTest {
 
     @Autowired
@@ -45,9 +52,8 @@ public class CustomerControllerTest {
     private RoleService roleService;
 
     @Test
-    @WithUserDetails(value = "admin", userDetailsServiceBeanName = "userDetailsService")
-    //@WithMockUser(username = "admin", roles { "ROLE_ADMIN})
-    public void givenAuthUser_whenGetOrderHistory_thenReturnOkStatusAndOrderHistoryView() throws Exception {
+    @WithMockUser(username = "admin", value = "admin", roles = {"ADMIN"})
+    public void whenGetOrderHistory_thenReturnOkStatusAndOrderHistoryView() throws Exception {
         User mockUser = mock(User.class);
         mockUser.setUserName("username");
         when(userService.findByUserName(anyString())).thenReturn(mockUser);
@@ -57,12 +63,12 @@ public class CustomerControllerTest {
                 .andExpect(view().name("customer/orderhistory"))
                 .andExpect(model().attribute("user", mockUser));
 
-        verify(userService).findByUserName("admin");
+        verify(userService).findByUserName(eq("admin"));
         verifyNoMoreInteractions(userService);
     }
 
     @Test
-    public void givenNoAuthUser_whenGetOrderHistory_thenReturnFoundStatusAndAccessDeniedView() throws Exception {
+    public void givenNoPrincipal_whenGetOrderHistory_thenReturnFoundStatusAndAccessDeniedView() throws Exception {
         mockMvc.perform(get("/customer/orderhistory"))
                 .andExpect(status().isFound())
                 .andExpect(view().name("access_denied"));
@@ -76,8 +82,7 @@ public class CustomerControllerTest {
     }
 
     @Test
-    @WithUserDetails(value = "admin", userDetailsServiceBeanName = "userDetailsService")
-    public void givenAuthUserAndValidCustomerForm_whenPostNewCustomer_thenReturnFoundStatusAndCreateNewCustomerToDbAndRedirectToLoginPage() throws Exception {
+    public void givenValidCustomerForm_whenPostNewCustomer_thenReturnFoundStatusAndCreateNewCustomerToDbAndRedirectToLoginPage() throws Exception {
         when(roleService.getById(anyInt())).thenReturn(mock(Role.class));
 
         mockMvc.perform(post("/customer/post")
@@ -93,20 +98,9 @@ public class CustomerControllerTest {
         verifyNoMoreInteractions(userService);
     }
 
-    @Test
-    @WithUserDetails(value = "user", userDetailsServiceBeanName = "userDetailsService")
-    public void givenAuthUserAndNoCsrfToken_whenPostNewCustomer_thenReturnForbiddenStatus() throws Exception {
-        mockMvc.perform(post("/customer/post")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .param("userName", "username")
-                .param("userEmail", "email")
-                .param("userPassword", "passwd"))
-                .andExpect(status().isForbidden());
-    }
 
     @Test
-    @WithUserDetails(value = "user", userDetailsServiceBeanName = "userDetailsService")
-    public void givenAuthUserAndInvalidUserForm_whenPostNewCustomer_thenReturnCustomerFormView() throws Exception {
+    public void givenInvalidUserForm_whenPostNewCustomer_thenReturnCustomerFormView() throws Exception {
         mockMvc.perform(post("/customer/post")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("userName", "")
@@ -117,34 +111,38 @@ public class CustomerControllerTest {
     }
 
     @Test
-    public void givenValidFormWithCsrfTokenAndNotAuthUser_whenPostNewCustomer_thenReturnForbiddenStatus() throws Exception {
-        mockMvc.perform(post("/customer/post")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .param("userName", "username")
-                .param("userEmail", "email")
-                .param("userPassword", "passwd").with(csrf()))
-                .andExpect(status().isForbidden());
-    }
-
-    @Test
-    @WithUserDetails(value = "user", userDetailsServiceBeanName = "userDetailsService")
-    public void givenAuthUser_whenGetCustomerSetting_thenReturnUserForm() throws Exception {
-        UserForm mockUserForm = mock(UserForm.class);
+    @WithMockUser(username = "admin", value = "admin", roles = {"ADMIN"})
+    public void givenPrincipal_whenGetCustomerSetting_thenReturnUserForm() throws Exception {
         User mockUser = mock(User.class);
+        mockUser.setUserName("admin");
         mockUser.setId(1);
-        when(userService.findByUserName("user")).thenReturn(mockUser);
-        when(userService.findUserFormById(1)).thenReturn(mockUserForm);
+
+        UserForm userFormMock = mock(UserForm.class);
+        userFormMock.setUserName("admin");
+        userFormMock.setUserPassword("passwd");
+        Address userBillingAddress = new Address();
+        userBillingAddress.setAddressLine1("address line 1");
+        userBillingAddress.setAddressLine2("address line 2");
+        userFormMock.setUserBillingAddress(userBillingAddress);
+
+        when(userService.findByUserName("admin")).thenReturn(mockUser);
+        when(userService.findUserFormById(anyInt())).thenReturn(userFormMock);
+
         mockMvc.perform(get("/customer/setting"))
                 .andExpect(status().isOk())
-                .andExpect(model().attribute("userForm", mockUserForm))
+                .andExpect(model().attribute("userForm", isNotNull()))
                 .andExpect(view().name("customer/customerform"));
+
+        ArgumentCaptor<UserForm> userFormArgumentCaptor = ArgumentCaptor.forClass(UserForm.class);
+        verify(userService, times(1)).findUserFormById(eq(1));
+        verify(userService, times(1)).findByUserName(eq("admin"));
+        verifyNoMoreInteractions(userService);
     }
 
     @Test
-    public void givenNotAuthUser_whenGetCustomerSetting_thenReturnAccessDeniedViewAndOkStatus() throws Exception {
+    public void givenNoPrincipal_whenGetCustomerSetting_thenReturnAccessDeniedViewAndOkStatus() throws Exception {
         mockMvc.perform(get("/customer/setting"))
                 .andExpect(view().name("access_denied"))
                 .andExpect(status().isOk());
     }
-
 }
