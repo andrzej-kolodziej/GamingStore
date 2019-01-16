8edd05aefe566e53bbf8983dd6a58d283d043bca
Andrzej Kolodziej <andrzej.kolodziej@motorolasolutions.com>
2019-01-16 12:00:15 +0100

    implement all developer controller tests

--- src/test/java/com/app/controllers/DeveloperControllerTest.java
+++ src/test/java/com/app/controllers/DeveloperControllerTest.java
@@ -24,7 +24,9 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
@@ -124,11 +126,6 @@ public class DeveloperControllerTest {
 
     @Test
     public void whenAddDeveloperProduct_thenReturnOkStatusAndAddDeveloperProductForm() throws Exception {
-/*        DeveloperForm developerForm = mock(DeveloperForm.class);
-        developerForm.setDeveloperName("name");
-        developerForm.setDeveloperImageUrl("url");
-        developerForm.setDeveloperDescription("description");*/
-
         mockMvc.perform(get("/developer?addProduct")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("developerName", "name")
@@ -137,13 +134,79 @@ public class DeveloperControllerTest {
                 .param("developerDescription", "description"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("developer/developerform"));
-
-        verify(developerForm, times(1)).getDeveloperProducts();
-        verifyNoMoreInteractions(developerForm);
     }
 
     @Test
     public void whenRemoveDeveloperProduct_thenReturnOkStatusAndDeveloperForm() throws Exception {
+        mockMvc.perform(get("/developer?removeProduct")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("developerName", "name")
+                .param("developerImageUrl", "url")
+                .param("developerId", "1")
+                .param("developerDescription", "description")
+                .requestAttr("removeProduct", "1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("developer/developerform"));
+    }
 
+    @Test
+    public void givenValidDeveloperForm_whenSaveDeveloper_thenReturnFoundStatusAndSaveDeveloperIntoDbAndRedirectToShowDeveloperPage() throws Exception {
+        Developer developer = new Developer();
+        developer.setName("name");
+        developer.setDescription("description");
+        developer.setId(1);
+        developer.setImageUrl("url");
+        when(developerService.saveOrUpdateDeveloperForm(any(DeveloperForm.class))).thenReturn(developer);
+
+        mockMvc.perform(post("/developer")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("developerName", "name")
+                .param("developerImageUrl", "url")
+                .param("developerId", "1")
+                .param("developerDescription", "description").with(csrf()))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/developer/show/1"));
+
+        verify(developerService, times(1)).saveOrUpdateDeveloperForm(any(DeveloperForm.class));
+        verifyNoMoreInteractions(developerService);
+    }
+
+    @Test
+    public void givenInvalidDeveloperForm_whenSaveDeveloper_thenReturnOkStatusAndSaveDeveloperIntoDbAndRedirectToShowDeveloperPage() throws Exception {
+        mockMvc.perform(post("/developer")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("developerName", "")
+                .param("developerImageUrl", "")
+                .param("developerId", "1")
+                .param("developerDescription", "").with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("developer/developerform"));
+    }
+
+    @Test
+    public void whenEditDeveloper_thenReturnOkStatusAndDeveloperFormView() throws Exception {
+        DeveloperForm developerForm = new DeveloperForm();
+        developerForm.setDeveloperName("name");
+        developerForm.setDeveloperDescription("description");
+        developerForm.setDeveloperId(1);
+        developerForm.setDeveloperImageUrl("url");
+        when(developerService.getDeveloperFormById(anyInt())).thenReturn(developerForm);
+
+        mockMvc.perform(get("/developer/edit/{id}", 1))
+                .andExpect(status().isOk())
+                .andExpect(view().name("developer/developerform"));
+
+        verify(developerService,  times(1)).getDeveloperFormById(anyInt());
+        verifyNoMoreInteractions(developerService);
+    }
+
+    @Test
+    public void whenDeleteDeveloper_thenRemoveDeveloperFromDbAndReturnFoundStatusAndRedirectToDeveloperList() throws Exception {
+        mockMvc.perform(get("/developer/delete/{id}", 1))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/developer/list"));
+
+        verify(developerService, times(1)).delete(1);
+        verifyNoMoreInteractions(developerService);
     }
 }
