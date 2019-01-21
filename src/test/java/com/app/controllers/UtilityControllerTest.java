8a01bf1ab2acbe07848e30513f6357dba0a7490e
Andrzej Kolodziej <andrzej.kolodziej@motorolasolutions.com>
2019-01-21 15:27:50 +0100

    utility controller unit test

--- src/test/java/com/app/controllers/UtilityControllerTest.java
+++ src/test/java/com/app/controllers/UtilityControllerTest.java
@@ -1,7 +1,10 @@
 package com.app.controllers;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,51 +12,33 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        classes = {/*SpringSecurityTestConfig.class,*/ /*SpringSecurityConfig.class*/})
-@TestPropertySource(
-        locations = "classpath:application.properties")
-@AutoConfigureMockMvc(secure = true)
 public class UtilityControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
+    @InjectMocks
+    private UtilityController utilityController;
 
-    @Test
-    public void whenGetAbout_thenReturnAboutView() throws Exception {
-        mockMvc.perform(get("/about"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("about"))
-                .andExpect(model().attribute("page", "about"));
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void whenGetWorkInProgress_thenReturnWorkInProgressView() throws Exception {
-        mockMvc.perform(get("/workinprogress"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("workinprogress"))
-                .andExpect(model().attribute("page", "about"));
+    public void whenGetUnderConstruction_thenReturnWorkInProgressView() {
+        String expectedView = "workinprogress";
+        String actualView = utilityController.underConstruction();
+        assertThat(actualView).isEqualTo(expectedView);
     }
 
     @Test
-    public void whenGetPrivacyPolicy_thenReturnWorkInProgressView() throws Exception {
-        mockMvc.perform(get("/privacy-policy"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("workinprogress"))
-                .andExpect(model().attribute("page", "about"));
-    }
-
-    @Test
-    public void whenGetUserAgreement_thenReturnWorkInProgressView() throws Exception {
-        mockMvc.perform(get("/user-agreement"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("workinprogress"))
-                .andExpect(model().attribute("page", "about"));
+    public void whenGetAbout_thenReturnWorkAboutView() {
+        String expectedView = "about";
+        String actualView = utilityController.doGet();
+        assertThat(actualView).isEqualTo(expectedView);
     }
 }
